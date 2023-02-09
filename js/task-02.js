dcfb649e64d9452279fd40ff23f0a947877c5984
nitskayl <anv1h3@gmail.com>
2023-02-09 12:27:47 +0200

    my_goit_js_hw_6

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+	'Potatoes',
+	'Mushrooms',
+	'Garlic',
+	'Tomatos',
+	'Herbs',
+	'Condiments',
+]
+
+const createLI = ingredients.map(el => {
+	const itemEl = document.createElement('li')
+	itemEl.textContent = el
+	itemEl.classList.add('item')
+	return itemEl
+})
+
+const getIngrELem = document.querySelector('#ingredients')
+
+getIngrELem.append(...createLI)
